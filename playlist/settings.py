7d2e8fb83c181cc38cc325ab0303d0c88c4af1cf
Vinicius José <vinijosenog@hotmail.com>
2025-06-25 14:27:06 -0300

    Added whitenoise dependency

--- playlist/settings.py
+++ playlist/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,3 +123,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 APPEND_SLASH = True
+
+STATIC_ROOT = "./static/"
