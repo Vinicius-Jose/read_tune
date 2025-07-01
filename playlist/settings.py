1753d06ca71c5785dbab3a8c3291b1d18dde40bf
Vinicius José <vinijosenog@hotmail.com>
2025-07-01 12:07:07 -0300

    Added allowed host

--- playlist/settings.py
+++ playlist/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if environ.get("DJANGO_DEBUG") == "True" else False
 
-ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "https://read-tune.onrender.com/"]
 
 
 # Application definition
